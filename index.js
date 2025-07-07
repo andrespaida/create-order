6000ca5cdd1ea0c3d0a0e2934d8d4a7475f5d92e
andrespaida <jordanpaida.bj@hotmail.com>
2025-07-07 18:20:58 -0500

    first deploy

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema.js';
 import resolvers from './resolver.js';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
-server.listen({ port: 4000 }).then(({ url }) => {
+server.listen({
+  port: 4000,
+  cors: {
+    origin: '*',
+  }
+}).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
